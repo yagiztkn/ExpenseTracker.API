73c98147749394eb92b1da66631c470412afab2d
Yağız Tekin <yagiz1905tekin@gmail.com>
2026-06-04 18:59:53 +0300

    feat: add CategoriesController, configure Swagger and fix db connection

---
+++ ExpenseTracker.API/Controllers/CategoriesController.cs
@@ -0,0 +1,36 @@
+﻿using ExpenseTracker.API.Data;
+using ExpenseTracker.API.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExpenseTracker.API.Controllers
+{
+    [Route("api/categories")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public CategoriesController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetCategories()
+        {
+            var categories = await _context.Categories.ToListAsync();
+            return Ok(categories);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddCategory(Category category)
+        {
+            _context.Categories.Add(category);
+            await _context.SaveChangesAsync();
+
+            return Ok(category);
+
+        }
+    }
+}
--- ExpenseTracker.API/Program.cs
+++ ExpenseTracker.API/Program.cs
@@ -1,30 +1,32 @@
 using ExpenseTracker.API.Data;
 using Microsoft.EntityFrameworkCore;
 
-
 var builder = WebApplication.CreateBuilder(args);
 
+// 1. Veritabanı köprümüzü kuruyoruz
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-// Add services to the container.
-
+// 2. Controller (Garson) altyapısını ekliyoruz
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+
+// 3. Swagger (Arayüz) test ekranı için gereken servisleri ekliyoruz
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// 4. Uygulama geliştirme aşamasındaysa Swagger ekranını kullanıma açıyoruz
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
 
+// 5. Yazdığımız CategoriesController gibi garsonları sahneye çağırıyoruz
 app.MapControllers();
 
 app.Run();
