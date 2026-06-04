c8c8b8d5cd8715a55c1513058ec2c8572c5d2558
Yağız Tekin <yagiz1905tekin@gmail.com>
2026-06-04 18:33:50 +0300

    feat: setup database context and generate initial migration

---
+++ ExpenseTracker.API/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using ExpenseTracker.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExpenseTracker.API.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Transaction> Transactions { get; set; }    
+    }
+}
--- ExpenseTracker.API/Program.cs
+++ ExpenseTracker.API/Program.cs
@@ -1,4 +1,11 @@
+using ExpenseTracker.API.Data;
+using Microsoft.EntityFrameworkCore;
+
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 // Add services to the container.
 
