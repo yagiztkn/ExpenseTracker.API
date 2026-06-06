2742fdbbecb14f2320852da37f4ec0a5588566ed
Yağız Tekin <yagiz1905tekin@gmail.com>
2026-06-06 18:06:32 +0300

    feat: add data validation rules to DTOs

--- ExpenseTracker.API/DTOs/CreateTransactionDto.cs
+++ ExpenseTracker.API/DTOs/CreateTransactionDto.cs
@@ -1,11 +1,23 @@
-﻿namespace ExpenseTracker.API.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ExpenseTracker.API.DTOs
 {
     public class CreateTransactionDto
     {
-
+        [Required(ErrorMessage = "Harcama Tutarı Girmek Zorunludur!")]
+        [Range(0.01, 1000000, ErrorMessage = "Harcama Tutarı 0'dan büyük olmalıdır!")]
         public decimal Amount { get; set; }
+
+
+        [MaxLength(100, ErrorMessage = "Harcama Açıklaması 100 karakterden fazla olamaz!")] 
         public string? Description { get; set; }
+
+
+        [Required(ErrorMessage = "Harcama Türü Seçmek Zorunludur!")]
         public int Type { get; set; }
+
+
+        [Required(ErrorMessage = "Harcama Kategorisi Seçmek Zorunludur!")]
         public int CategoryId { get; set; }
     }
 }
